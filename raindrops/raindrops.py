18c0b8914adcf5e8578e321d27185edd1ed1b728
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-02 12:38:28 +1100

    Implemented raindrops functionality

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,17 @@
+# NOTE: Define divisors and corresponding raindrop sounds here
+RAINDROPS = {
+    3: "Pling",
+    5: "Plang",
+    7: "Plong"
+}
+
 def convert(number):
-    pass
+    output = ""
+    divisors = sorted([d for d in RAINDROPS.keys()])
+
+    # Iterate through divisors and append sounds for each that divides 'number'
+    for div in divisors:
+        if number % div == 0:
+            output += RAINDROPS[div]
+    
+    return(output if output != "" else str(number))
