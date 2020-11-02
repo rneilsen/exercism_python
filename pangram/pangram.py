6a8b67629b255a1ff6512b1997903e88c2244fb4
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-02 12:52:55 +1100

    Implemented is_pangram() function

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,10 @@
+import string
+
 def is_pangram(sentence):
-    pass
+    used_chars = set(sentence.lower())
+    
+    for c in string.ascii_lowercase:
+        if c not in used_chars:
+            return False
+    
+    return True
