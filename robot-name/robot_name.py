d3926de692c9face9a00554ca77e6fb44d49ce72
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-03 17:02:44 +1100

    Functional implementation of Robot class with init and reset functions

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,20 @@
+import string, random
+
 class Robot:
+    usednames = set([""])
+
     def __init__(self):
-        pass
+        self.name = self.newname()
+    
+    def reset(self):
+        self.name = self.newname()
+
+    def newname(self):
+        name = ""
+        while name in Robot.usednames:
+            # Name schema: two random uppercase letters, three random digits
+            name = random.choice(string.ascii_uppercase) \
+                    + random.choice(string.ascii_uppercase) \
+                    + str(random.randint(0,999)).zfill(3)
+        Robot.usednames.add(name)
+        return name
