21edbaea63fd92efa445c2cd7d85c23fba916ad2
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-04 23:52:12 +1100

    Implemented classify function

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,2 +1,14 @@
+from sympy import divisors
+
 def classify(number):
-    pass
+    if number < 1:
+        raise ValueError("Input must be positive integer")
+    
+    divsum = sum(divisors(number)[:-1])
+    if divsum < number:
+        return "deficient"
+    elif divsum > number:
+        return "abundant"
+    else:
+        return "perfect"
+    
