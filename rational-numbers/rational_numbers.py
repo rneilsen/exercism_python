5b2b842d07c4c9bcc2a129f50ab37a7613c4ed63
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-06 00:54:04 +1100

    Implemented rational_numbers class and related methods

--- rational-numbers/rational_numbers.py
+++ rational-numbers/rational_numbers.py
@@ -1,10 +1,17 @@
 from __future__ import division
+from sympy import gcd
 
 
 class Rational:
     def __init__(self, numer, denom):
-        self.numer = None
-        self.denom = None
+        if denom == 0:
+            raise ValueError("Cannot have a 0 denominator")
+
+        d = gcd(numer, denom)
+        if denom < 0:
+            d = -d
+        self.numer = numer / d
+        self.denom = denom / d
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
@@ -13,22 +20,24 @@ class Rational:
         return '{}/{}'.format(self.numer, self.denom)
 
     def __add__(self, other):
-        pass
+        return Rational(self.numer * other.denom + self.denom * other.numer,
+                        self.denom * other.denom)
 
     def __sub__(self, other):
-        pass
+        return Rational(self.numer * other.denom - self.denom * other.numer,
+                        self.denom * other.denom)
 
     def __mul__(self, other):
-        pass
+        return Rational(self.numer * other.numer, self.denom * other.denom)
 
     def __truediv__(self, other):
-        pass
+        return Rational(self.numer * other.denom, self.denom * other.numer)
 
     def __abs__(self):
-        pass
+        return Rational(abs(self.numer), abs(self.denom))
 
     def __pow__(self, power):
-        pass
+        return Rational(self.numer ** power, self.denom ** power)
 
     def __rpow__(self, base):
-        pass
+        return base ** (self.numer / self.denom)
