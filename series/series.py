e896ade945b2493d2b704b0d649d6c8f4a800c84
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-06 00:34:49 +1100

    Implemented slices function

--- series/series.py
+++ series/series.py
@@ -1,2 +1,9 @@
 def slices(series, length):
-    pass
+    if length > len(series):
+        raise ValueError("slice length cannot be longer than series length")
+    if length < 1:
+        raise ValueError("slice length cannot be less than 1")
+    if len(series) == 0:
+        raise ValueError("series cannot be empty")
+    
+    return [series[i:i+length] for i in range(len(series) - length + 1)]
