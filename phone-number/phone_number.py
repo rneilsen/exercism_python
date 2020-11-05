ee6d704889c312ad2e91ecf2046c6f8cff4780f1
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-06 00:29:02 +1100

    Implemented PhoneNumber class init and pretty methods

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -1,3 +1,18 @@
 class PhoneNumber:
     def __init__(self, number):
-        pass
+        scrubbed = ''.join(filter(lambda x: x.isdigit(), number))
+        if scrubbed[0] == '1':
+            scrubbed = scrubbed[1:]
+        
+        if len(scrubbed) != 10:
+            raise ValueError("NANP numbers must be 10 digits long")
+        if int(scrubbed[0]) not in range(2,10):
+            raise ValueError("Area code must start with digit 2-9")
+        if int(scrubbed[3]) not in range(2,10):
+            raise ValueError("Exchange code must start with digit 2-9")
+        
+        self.number = scrubbed
+        self.area_code = scrubbed[:3]
+    
+    def pretty(self):
+        return f"({self.number[:3]})-{self.number[3:6]}-{self.number[6:]}"
