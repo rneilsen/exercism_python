1ef987a2405f910e7ee9cf8fe17057382e6dce79
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-06 12:50:34 +1100

    Cleaned init method, implemented copy() in row() method to match behaviour of column()

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,10 +1,10 @@
 class Matrix:
     def __init__(self, matrix_string):
         self.rows = [ [int(c) for c in r.split()]
-                      for r in matrix_string.split('\n') ]
+                      for r in matrix_string.splitlines() ]
 
     def row(self, index):
-        return self.rows[index-1]
+        return self.rows[index-1].copy()
 
     def column(self, index):
         return [ r[index-1] for r in self.rows ]
