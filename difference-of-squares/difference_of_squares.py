7f265195544688c1e3396a3f6b275564ddb7a42e
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-09 00:56:39 +1100

    Implemented square_of_sum, sum_of_squares, difference_of_squares functions

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,12 @@
 def square_of_sum(number):
-    pass
+    # square of (n^2+n)/2 formula
+    return ((number**2 + number) / 2)**2
 
 
 def sum_of_squares(number):
-    pass
+    # closed form of square pyramidal number
+    return number * (number+1) * (2*number+1) / 6
 
 
 def difference_of_squares(number):
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
