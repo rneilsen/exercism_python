c716832229ff44ae5cd7628fc6ddf6828554d9c4
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-09 00:47:28 +1100

    Implemented is_valid function

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,2 +1,19 @@
 def is_valid(isbn):
-    pass
+    scrubbed = list(filter(lambda x: x != '-', isbn))
+
+    if len(scrubbed) != 10:
+        return False
+    for c in scrubbed[:-1]:
+        if not c.isdigit():
+            return False
+    
+    digits = [int(c) for c in scrubbed[:-1]]
+    
+    if scrubbed[-1].isdigit():
+        digits.append(int(scrubbed[-1]))
+    elif scrubbed[-1] == 'X':
+        digits.append(10)
+    else:
+        return False
+    
+    return (sum([x*i for (x,i) in zip(digits, range(10,0,-1))]) % 11 == 0)
