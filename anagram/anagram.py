bf07c020e53710377200042d840d268b344f384e
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-09 01:08:35 +1100

    Implemented find_anagrams function

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,9 @@
 def find_anagrams(word, candidates):
-    pass
+    chars = sorted(list(word.lower()))
+
+    matches = []
+    for c in candidates:
+        if sorted(list(c.lower())) == chars and c.lower() != word.lower():
+            matches.append(c)
+    
+    return matches
