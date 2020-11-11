cc6166fa6ae19773a5a5300fb88feaab5f2c844f
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-12 02:49:34 +1100

    Implemented first three error handling techniques (filelike object not yet implemented)

--- error-handling/error_handling.py
+++ error-handling/error_handling.py
@@ -1,13 +1,19 @@
 def handle_error_by_throwing_exception():
-    pass
+    raise Exception("A meaningful message would go here")
 
 
 def handle_error_by_returning_none(input_data):
-    pass
+    if input_data.isnumeric():
+        return int(input_data)
+    else:
+        return None
 
 
 def handle_error_by_returning_tuple(input_data):
-    pass
+    if input_data.isnumeric():
+        return (True, int(input_data))
+    else:
+        return (False, input_data)
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
