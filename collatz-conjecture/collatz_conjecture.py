076c5eceebe328586ffa9a3f5d1f49c87a85073e
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-12 01:51:11 +1100

    Implemented steps() function

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    if number < 1:
+        raise ValueError("Positive integers only")
+    return collatz(number)
+
+
+def collatz(number):
+    if number == 1:
+        return 0
+    elif number % 2 == 0:   # even
+        return 1 + collatz(number / 2)
+    else:   # odd
+        return 1 + collatz(3*number + 1)
