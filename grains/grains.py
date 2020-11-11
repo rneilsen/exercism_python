759648bdfdfaf450018de8734577cfc20bfc67dd
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-12 01:57:19 +1100

    Implemented square() and total() functions

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,8 @@
 def square(number):
-    pass
+    if number < 1 or number > 64:
+        raise ValueError('Squares are numbered 1-64')
+    return 2**(number-1)
 
 
 def total():
-    pass
+    return 2**64 - 1    # sum of 2^i for i in [0,1,2,...,n-1] equals 2^n - 1
