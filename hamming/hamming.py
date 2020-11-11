306338a20858bfdf2f24c471e4cd0b0328a38d19
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-11 14:17:01 +1100

    Nicer implementation of hamming() using a list comp

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,8 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("String lengths must be the same")
-    dist = 0
-    for (a_char, b_char) in zip(strand_a, strand_b):
-        if a_char != b_char:
-            dist += 1
-    return dist
+    return sum([1 for (a,b) in zip(strand_a, strand_b) if a!=b])
