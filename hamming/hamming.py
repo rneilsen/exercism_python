e08e1586d6b10125a61ddb8323cdea30bba8b541
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-12 00:25:38 +1100

    Replaced sum() with len() for slight performance improvement

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("String lengths must be the same")
-    return sum([1 for (a,b) in zip(strand_a, strand_b) if a!=b])
+    return len([1 for (a,b) in zip(strand_a, strand_b) if a != b])
