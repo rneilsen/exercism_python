e34585b40ea9ee4147a97147a3263b3bb6f93c80
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-12 02:59:35 +1100

    Implemented filelike objects exception technique using try/except

--- error-handling/error_handling.py
+++ error-handling/error_handling.py
@@ -17,4 +17,10 @@ def handle_error_by_returning_tuple(input_data):
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
-    pass
+    filelike_object.open()
+    try:
+        filelike_object.do_something()
+    except:
+        filelike_object.close()
+        raise Exception("Doing something failed.")
+    filelike_object.close()
