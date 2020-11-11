a7200aae02e1b34e79e90e22470dc46b6808a7cb
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-12 02:39:35 +1100

    Implemented factors() manually (not cheating this time)

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,4 +1,27 @@
-from sympy import factorint
-
 def factors(value):
-    return factorint(value, multiple=True)
+    primes = []
+    n = 1
+
+    prime_factors = []
+    remainder = value
+    while remainder > 1:
+        n += 1
+        n_prime = True
+        # test if n is prime using current known primes
+        for p in primes:
+            if n % p == 0:
+                n_prime = False
+                break
+            if p*p > n:
+                # no need to keep testing primes once we pass sqrt(n)
+                break   
+        
+        if n_prime:
+            primes.append(n)
+            
+            while remainder % n == 0:
+                # divide out all copies of the new prime from remainder
+                remainder = remainder // n
+                prime_factors.append(n)
+
+    return prime_factors
