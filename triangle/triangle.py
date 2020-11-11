70ec7091ce2707934a32176b0b37bc7ec194bc6f
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-12 01:45:51 +1100

    Implemented equilateral(), isosceles(), scalene(), degenerate() functions

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,28 @@
 def equilateral(sides):
-    pass
+    if not test_valid(sides): return False
+    return len(set(sides)) == 1
 
 
 def isosceles(sides):
-    pass
+    if not test_valid(sides): return False
+    return len(set(sides)) <= 2
 
 
 def scalene(sides):
-    pass
+    if not test_valid(sides): return False
+    return len(set(sides)) == 3
+
+
+def degenerate(sides):
+    if not test_valid(sides): return False
+    s_sides = sides.sort()
+    return s_sides[0] + s_sides[1] == s_sides[2]
+
+
+def test_valid(sides):
+    if min(sides) <= 0:
+        return False        
+    s_sides = sorted(sides)
+    if s_sides[0] + s_sides[1] < s_sides[2]:
+        return False
+    return True
