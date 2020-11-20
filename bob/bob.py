60d119627738f17d33f5a007007c4ed2a6a0eb57
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-21 02:51:21 +1100

    Implemented response function

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,22 @@
 def response(hey_bob):
-    pass
+    cleaned = hey_bob.strip()
+    (question, yelling) = (False, False)
+    
+    if len(cleaned) == 0:
+        # Catch empty strings first
+        return "Fine. Be that way!"
+
+    if cleaned[-1] == '?':
+        question = True
+    if cleaned.isupper():
+        yelling = True
+
+    if question and yelling:
+        return "Calm down, I know what I'm doing!"
+    elif question:
+        return "Sure." 
+    elif yelling:
+        return "Whoa, chill out!"
+    else: 
+        return "Whatever."
+
