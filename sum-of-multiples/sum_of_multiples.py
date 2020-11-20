ac857d32550668dfd1544c0428a185fa172d653f
Richard Neilsen <RNeilsen@users.noreply.github.com>
2020-11-21 03:04:05 +1100

    Implemented sum_of_multiples() function

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,15 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    used_mults = set()
+    sum = 0
+
+    for n in multiples:
+        if n == 0:
+            continue
+        i = n
+        while i < limit:
+            if i not in used_mults:
+                sum += i
+                used_mults.add(i)
+            i += n
+    
+    return sum
