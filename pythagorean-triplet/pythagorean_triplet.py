1598d1751d77403d2cc5d667b0365b20880cf3c8
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-06-24 16:16:22 +1000

    Implemented triplets_with_sum (slow!)

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,2 +1,8 @@
 def triplets_with_sum(number):
-    pass
+    triplets = []
+    for a in range(1, number // 3):
+        for b in range(a+1, 2 * (number // 3)):
+            c = number - (a+b)
+            if a**2 + b**2 == c**2:
+                triplets.append([a,b,c])
+    return triplets
