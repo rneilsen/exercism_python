f1aa7b15ad342485615af4970570ec535813a27a
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-06-24 15:54:43 +1000

    Implemented count_words function using regeps

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,11 @@
+from collections import Counter
+import re
+
 def count_words(sentence):
-    pass
+    # note: regex needs the contraction pattern [a-z]+\'[a-z]+ before the
+    # simple word pattern [a-z]+ to ensure it finds contractions correctly
+    wordlist = re.findall('([0-9]+|[a-z]+\'[a-z]+|[a-z]+)', sentence.strip().lower())
+    word_counts = Counter()
+    for w in wordlist:
+        word_counts[w] += 1
+    return word_counts
