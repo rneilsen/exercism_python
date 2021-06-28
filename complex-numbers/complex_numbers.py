17d1071ad6873b8840883d439b5924fad566016d
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-06-28 18:11:39 +1000

    Implemented ComplexNumber class

--- complex-numbers/complex_numbers.py
+++ complex-numbers/complex_numbers.py
@@ -1,27 +1,44 @@
+from math import sqrt, exp, cos, sin
+
 class ComplexNumber:
     def __init__(self, real, imaginary):
-        pass
+        (self.real, self.imaginary) = (real, imaginary)
 
     def __eq__(self, other):
-        pass
+        return (self.real == other.real and self.imaginary == other.imaginary)
 
     def __add__(self, other):
-        pass
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
 
     def __mul__(self, other):
-        pass
+        return ComplexNumber(
+                self.real * other.real - self.imaginary * other.imaginary,
+                self.real * other.imaginary + self.imaginary * other.real
+        )
 
     def __sub__(self, other):
-        pass
+        return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
 
     def __truediv__(self, other):
-        pass
+        return self * other.reciprocal()
 
     def __abs__(self):
-        pass
+        return sqrt(self.real**2 + self.imaginary**2)
+
+    def __repr__(self):
+        return f"{self.real} {('+' if self.imaginary >= 0 else '-')} {abs(self.imaginary)}i"
 
     def conjugate(self):
-        pass
+        return ComplexNumber(self.real, -self.imaginary)
+
+    def reciprocal(self):
+        return ComplexNumber(
+                self.real / (self.real**2 + self.imaginary**2),
+                - self.imaginary / (self.real**2 + self.imaginary**2)
+        )
 
     def exp(self):
-        pass
+        return ComplexNumber(
+                exp(self.real) * cos(self.imaginary), 
+                exp(self.real) * sin(self.imaginary)
+        )
