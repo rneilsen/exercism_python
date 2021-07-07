68c80f5fe10f909221ac9ddf9edcc7717349eef4
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-07-07 17:03:16 +1000

    All tests except last one working

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -1,9 +1,30 @@
+from secrets import randbelow
+from itertools import cycle
+from string import ascii_lowercase
+
+DEFAULT_KEY_LENGTH = 100
+
 class Cipher:
     def __init__(self, key=None):
-        pass
+        if key is not None:
+            self.key = key.lower()
+        else:
+            self.key = ''
+            for n in range(DEFAULT_KEY_LENGTH):
+                self.key += ascii_lowercase[randbelow(26)]
+      
+        self.cycler = cycle([ord(c) - ord('a') for c in self.key])
 
     def encode(self, text):
-        pass
+        ciphertext = ''
+        for c in text:
+            mod = (ord(c) - ord('a') + next(self.cycler)) % 26
+            ciphertext += chr(ord('a') + mod)
+        return ciphertext
 
     def decode(self, text):
-        pass
+        plaintext = ''
+        for c in text:
+            mod = (ord(c) - ord('a') - next(self.cycler)) % 26
+            plaintext += chr(ord('a') + mod)
+        return plaintext
