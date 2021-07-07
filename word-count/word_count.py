21e143864545f35d1bb28f515bd6a52b6a12d799
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-07-07 16:12:55 +1000

    Removed outdated comment and cleaned up regex by using "" strings

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -2,8 +2,6 @@ from collections import Counter
 import re
 
 def count_words(sentence):
-    # note: regex needs the contraction pattern [a-z]+\'[a-z]+ before the
-    # simple word pattern [a-z]+ to ensure it finds contractions correctly
-    word_list = re.findall('[0-9a-z]+(?:\'[a-z]+)?', sentence.strip().lower())
+    word_list = re.findall("[0-9a-z]+(?:'[a-z]+)?", sentence.strip().lower())
     word_counts = Counter(word_list)
     return word_counts
