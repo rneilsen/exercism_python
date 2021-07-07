16648219a19d4d68ff7bdd6b946968d937dd010d
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-07-07 16:23:46 +1000

    Implemented score() function

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,18 @@
+score_groups = {
+        1:  'aeioulnrst',
+        2:  'dg',
+        3:  'bcmp',
+        4:  'fhvwy',
+        5:  'k',
+        8:  'jx',
+        10: 'qz'
+    }
+
+# builds a dict of per-letter scores from the above human-convenient score list
+scores = {}
+for score_value in score_groups.keys():
+    for letter in score_groups[score_value]:
+        scores[letter] = score_value
+
 def score(word):
-    pass
+    return sum([scores[l] for l in word.lower()])
