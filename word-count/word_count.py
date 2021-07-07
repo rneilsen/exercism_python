5afd2d8df9a0571ecc7210b7c8a129e11c1ed8a3
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-07-07 16:10:35 +1000

    Streamlined Counter code and added cleaner regex for finding words

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -4,8 +4,6 @@ import re
 def count_words(sentence):
     # note: regex needs the contraction pattern [a-z]+\'[a-z]+ before the
     # simple word pattern [a-z]+ to ensure it finds contractions correctly
-    wordlist = re.findall('([0-9]+|[a-z]+\'[a-z]+|[a-z]+)', sentence.strip().lower())
-    word_counts = Counter()
-    for w in wordlist:
-        word_counts[w] += 1
+    word_list = re.findall('[0-9a-z]+(?:\'[a-z]+)?', sentence.strip().lower())
+    word_counts = Counter(word_list)
     return word_counts
