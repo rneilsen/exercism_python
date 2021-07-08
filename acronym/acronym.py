792c199feec0b999a7ccdfa401eb34285a736a65
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-07-08 15:39:54 +1000

    Slight clean up, now all uppercase, all the time

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,5 +1,5 @@
 import re
 
 def abbreviate(words):
-    word_list = re.findall("[0-9a-z]+(?:'[a-z]+)?", words.strip().lower())
-    return ''.join([w[0] for w in word_list]).upper()
+    word_list = re.findall("[0-9A-Z]+(?:'[A-Z]+)?", words.strip().upper())
+    return ''.join([w[0] for w in word_list])
