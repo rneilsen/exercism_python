890141af4e513a440b8581c92723eac9a03b06d1
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-07-09 15:12:58 +1000

    Implemented Garden class init and plants methods

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,21 @@
+DEFAULT_STUDENTS = ['Alice', 'Bob', 'Charlie', 'David',
+            'Eve', 'Fred', 'Ginny', 'Harriet',
+            'Ileana', 'Joseph', 'Kincaid', 'Larry']
+
+PLANTS = { 'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'}
+
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    def __init__(self, diagram, students=DEFAULT_STUDENTS):
+        self.students = sorted(students)
+        self.rows = diagram.split()
+
+    def plants(self, student):
+        try:
+            pos = self.students.index(student) * 2
+        except:
+            raise Exception(student + " is not in the list of students")
+        
+        student_plants = []
+        for row in self.rows:
+            student_plants.extend([PLANTS[c] for c in row[pos:pos+2]])
+        return student_plants
