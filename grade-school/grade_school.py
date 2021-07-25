cc61227787dfd149e05b1addebbd5bc159de0b6e
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-07-25 17:31:15 +1000

    Implemented School class with req'd methods

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,12 +1,18 @@
 class School:
     def __init__(self):
-        pass
+        self.grades = {}        # key = grade (int), val = students (set of strings)
 
     def add_student(self, name, grade):
-        pass
+        if grade not in self.grades:
+            self.grades[grade] = {name}
+        else:
+            self.grades[grade].add(name)
 
     def roster(self):
-        pass
+        roster = []
+        for grade in sorted(self.grades.keys()):
+            roster += sorted(self.grades[grade])
+        return roster
 
     def grade(self, grade_number):
-        pass
+        return sorted(self.grades.get(grade_number, []))
