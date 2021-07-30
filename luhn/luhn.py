b17dd5f73b94c8c6ac1e6454536a1a070d29bdde
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-07-30 12:50:43 +1000

    Used try...except to streamline code

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -4,17 +4,17 @@ class Luhn:
         
     def valid(self):
         chars = list(self.num.strip().replace(' ', ''))
-        
         if len(chars) <= 1:
             return False
-        for ch in chars:
-            if not ch.isdigit(): return False
-
+        try:
+            digits = [int(ch) for ch in chars]
+        except ValueError:
+            return False
+        
         def rec_luhn_sum(digits):
             if len(digits) < 2:
                 return sum(digits)
             return (digits[-1] + 2*digits[-2] - (9 if digits[-2] >= 5 else 0) +
                     rec_luhn_sum(digits[:len(digits) - 2]))        
 
-        digits = [int(ch) for ch in chars]
         return (rec_luhn_sum(digits) % 10 == 0)
