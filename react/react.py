bdee3a1066c9e59ad6dc69c17d0fefb678aabddb
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-03 15:23:08 +1000

    Some basic functionality, passing 5/14 tests

--- react/react.py
+++ react/react.py
@@ -1,11 +1,11 @@
 class InputCell:
     def __init__(self, initial_value):
-        self.value = None
-
+        self.value = initial_value
+    
 
 class ComputeCell:
     def __init__(self, inputs, compute_function):
-        self.value = None
+        self.value = compute_function([inp.value for inp in inputs])
 
     def add_callback(self, callback):
         pass
