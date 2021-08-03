b59e175678d4dd82e795f348173ea7777af17b30
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-03 17:34:01 +1000

    Passes all non-callback tests

--- react/react.py
+++ react/react.py
@@ -1,11 +1,32 @@
 class InputCell:
     def __init__(self, initial_value):
+        self.dependents = set()
         self.value = initial_value
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        self._value = value
+        for dep in self.dependents:
+            dep.compute()
     
 
 class ComputeCell:
     def __init__(self, inputs, compute_function):
-        self.value = compute_function([inp.value for inp in inputs])
+        self.inputs = inputs
+        self.function = compute_function
+        self.dependents = set()
+        for inp in inputs:
+            inp.dependents.add(self)
+        self.compute()
+
+    def compute(self):
+        self.value = self.function([inp.value for inp in self.inputs])
+        for dep in self.dependents:
+            dep.compute()
 
     def add_callback(self, callback):
         pass
