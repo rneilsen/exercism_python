cf3b61f7a77b509b81131ee4bbbd2531a919fba0
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-04 14:15:30 +1000

    Implemented Clock class, passes all tests

--- clock/clock.py
+++ clock/clock.py
@@ -1,15 +1,16 @@
 class Clock:
     def __init__(self, hour, minute):
-        pass
+        self.hour = (hour + minute // 60) % 24
+        self.minute = minute % 60
 
     def __repr__(self):
-        pass
+        return str(self.hour).zfill(2) + ':' + str(self.minute).zfill(2)
 
     def __eq__(self, other):
-        pass
+        return (self.hour == other.hour and self.minute == other.minute)
 
     def __add__(self, minutes):
-        pass
+        return Clock(self.hour, self.minute + minutes)
 
     def __sub__(self, minutes):
-        pass
+        return Clock(self.hour, self.minute - minutes)
