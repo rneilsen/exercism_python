6d7a34c0f0b59951a800af4f88e13a5748f26485
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-04 14:55:14 +1000

    Implemented callback handling

--- react/react.py
+++ react/react.py
@@ -12,6 +12,8 @@ class InputCell:
         self._value = value
         for dep in self.dependents:
             dep.compute()
+        for dep in self.dependents:
+            dep.check_for_callbacks()
     
 
 class ComputeCell:
@@ -19,17 +21,31 @@ class ComputeCell:
         self.inputs = inputs
         self.function = compute_function
         self.dependents = set()
+        self.callbacks = set()
+        self.prev_value = self.value = None
         for inp in inputs:
             inp.dependents.add(self)
         self.compute()
+        self.check_for_callbacks()
 
     def compute(self):
         self.value = self.function([inp.value for inp in self.inputs])
         for dep in self.dependents:
             dep.compute()
+        
+    def check_for_callbacks(self):
+        if self.value != self.prev_value:
+            self.prev_value = self.value
+            self.run_callbacks()
+            for dep in self.dependents:
+                dep.check_for_callbacks()
 
     def add_callback(self, callback):
-        pass
+        self.callbacks.add(callback)
 
     def remove_callback(self, callback):
-        pass
+        self.callbacks.discard(callback)
+
+    def run_callbacks(self):
+        for cb in self.callbacks:
+            cb(self.value)
