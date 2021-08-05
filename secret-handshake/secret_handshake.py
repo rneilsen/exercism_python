673e86ba3fed30cde97d62ac11c5e638998a379f
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-05 16:51:38 +1000

    Implemented commands() function

--- secret-handshake/secret_handshake.py
+++ secret-handshake/secret_handshake.py
@@ -1,2 +1,13 @@
+GESTURES = {0:  'wink', 
+            1:  'double blink',
+            2:  'close your eyes',
+            3:  'jump'}
+
 def commands(number):
-    pass
+    handshake = []
+    for n in GESTURES.keys():
+        if (number // 2**n) % 2:
+            handshake.append(GESTURES[n])
+    if (number // 16) % 2:
+        handshake.reverse()
+    return handshake
