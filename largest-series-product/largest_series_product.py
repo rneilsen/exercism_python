57161a1536f326da98d10df45f0a452404f01082
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-05 17:11:07 +1000

    Implemented largest_product function

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,2 +1,8 @@
+from math import prod
+
 def largest_product(series, size):
-    pass
+    if size < 0 or size > len(series):
+        raise ValueError("Invalid size given")
+    digits = [int(ch) for ch in series]
+    return max( [ prod( digits[i:i+size] ) 
+            for i in range(len(digits) - size + 1) ] )
