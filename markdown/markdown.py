92df7de180533f8e3d28e9206e157bd0368692a5
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-10 14:46:34 +1000

    Removed now-superfluous bold and italics code

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -25,36 +25,10 @@ def parse(markdown):
         if m:
             if not in_list:
                 in_list = True
-                is_bold = False
-                is_italic = False
                 curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                    is_italic = True
                 line = '<ul><li>' + curr + '</li>'
             else:
-                is_bold = False
-                is_italic = False
                 curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    is_italic = True
-                if is_bold:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                if is_italic:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
                 line = '<li>' + curr + '</li>'
         else:
             if in_list:
@@ -64,12 +38,7 @@ def parse(markdown):
         m = re.match('<h|<ul|<p|<li', line)
         if not m:
             line = '<p>' + line + '</p>'
-        m = re.match('(.*)__(.*)__(.*)', line)
-        if m:
-            line = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
-        m = re.match('(.*)_(.*)_(.*)', line)
-        if m:
-            line = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
+        
         if in_list_append:
             line = '</ul>' + line
             in_list_append = False
