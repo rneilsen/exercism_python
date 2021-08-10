72273ff8c4650f0c0accca729cfb670eb80b026c
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-10 15:26:53 +1000

    Minor cleanups

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -8,30 +8,25 @@ def parse(markdown):
     in_list_append = False
     for line in lines:
         # Detect headers
-        m = re.match('(#+) (.*)', line)
-        if m:
+        if ( m := re.match('(#+) (.*)', line) ):
             ht = str(len(m.group(1)))
             line = f'<h{ht}>' + m.group(2) + f'</h{ht}>'
 
         # Detect bold
-        while m := re.match('(.*)__(.*?)__(.*)', line):
+        while ( m := re.match('(.*)__(.*?)__(.*)', line) ):
             line = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
         
         # Detect italic
-        while m := re.match('(.*)_(.*?)_(.*)', line):
+        while ( m := re.match('(.*)_(.*?)_(.*)', line) ):
             line = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
         
         m = re.match(r'\* (.*)', line)
         if m:
+            line = '<li>' + m.group(1) + '</li>'
             if not in_list:
                 in_list = True
-                curr = m.group(1)
-                line = '<ul><li>' + curr + '</li>'
-            else:
-                curr = m.group(1)
-                line = '<li>' + curr + '</li>'
-        else:
-            if in_list:
+                line = '<ul>' + line
+        elif in_list:
                 in_list_append = True
                 in_list = False
 
