1407761f5e156c0e4580c66a10719c7aa96ec749
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-17 15:00:03 +1000

    Sort primarily by points (desc), secondarily by teamname (asc)

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -2,7 +2,7 @@ class TeamEntry:
     def __init__(self):
         self.wins = self.losses = self.draws = 0
 
-    def get_scores(self):   # return tuple: (MP, W, D, L, P)
+    def get_scores(self):   # return tuple of ints: (MP, W, D, L, P)
         return (self.wins + self.losses + self.draws,
                 self.wins, self.draws, self.losses,
                 3 * self.wins + 1 * self.draws)
@@ -10,13 +10,11 @@ class TeamEntry:
 
 class Tournament:
     def __init__(self):
-        self.teams = {}
+        self.teams = {}     # key = teamname (str), val = TeamEntry()
     
     def add_result(self, ta, tb, result):
-        if ta not in self.teams:
-            self.teams[ta] = TeamEntry()
-        if tb not in self.teams:
-            self.teams[tb] = TeamEntry()
+        if ta not in self.teams: self.teams[ta] = TeamEntry()
+        if tb not in self.teams: self.teams[tb] = TeamEntry()
 
         if result == 'draw':
             self.teams[ta].draws += 1
@@ -24,12 +22,12 @@ class Tournament:
             return
         
         if result == 'loss':
-            ta, tb = tb, ta     # swap places and count as a win
+            ta, tb = tb, ta     # swap ta, tb and process as if result = 'win'
         
         self.teams[ta].wins += 1
         self.teams[tb].losses += 1
 
-    def all_results(self):      # return dict: {teamname : (MP, W, D, L, P)}
+    def all_results(self):      # return dict: { teamname : (MP, W, D, L, P) }
         return {team : self.teams[team].get_scores() for team in self.teams}
 
 
@@ -38,10 +36,14 @@ def tally(rows):
     for row in rows:
         ta, tb, res = row.split(';')
         tourn.add_result(ta, tb, res)
-    results = tourn.all_results()
+
+    results = [(k, v) for k, v in tourn.all_results().items()]
+    # sort by teamname (asc), then by points (desc)
+    results.sort(key=lambda x: x[0])
+    results.sort(key=lambda x: x[1][4], reverse=True)
+
     table_rows = [('Team', ('MP', 'W', 'D', 'L', 'P'))]
-    table_rows += [ (t, sc) for t, sc in 
-            sorted(results.items(), key=lambda x: x[1][4], reverse=True)]
+    table_rows += results
     
     disp_results = []
     for row in table_rows:
@@ -53,5 +55,4 @@ def tally(rows):
         disp_results.append(disp_row)
 
     return disp_results
-
             
