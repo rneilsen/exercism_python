3c354cd16fcdaf1d8177ee41ec02ba28dd4f60a6
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-18 17:14:11 +1000

    Check for rectangle board shape

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,3 +1,6 @@
 def annotate(minefield):
     # Function body starts here
-    pass
+    lengths = [len(row) for row in minefield]
+    if max(lengths) != min(lengths):
+        raise ValueError("Invalid board - rows not equal length")
+    
