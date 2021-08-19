c042ae05a6d589df710ff576d4cddaebaa1e2567
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-19 15:13:33 +1000

    Implement basic functionality and error checking

--- wordy/wordy.py
+++ wordy/wordy.py
@@ -1,3 +1,24 @@
-def answer(question: str):
-    pass
+from re import sub
 
+ops = { 'plus'          : '+' , 
+        'minus'         : '-' ,
+        'multiplied by' : '*' , 
+        'divided by'    : '//' }
+
+def answer(question):
+    if not ( question.startswith('What is') and question[-1] == '?' ):
+        raise ValueError("Invalid question format")
+    
+    for k,v in ops.items():
+        question = sub(k, v, question)
+    
+    pieces = question[8:-1].split(' ')
+
+    while len(pieces) > 1:
+        if len(pieces) < 3:
+            raise ValueError("Unknown question")
+        
+        pieces = [str(eval(''.join(pieces[0:3])))] + pieces[3:]
+
+    return int(pieces[0])
+    
