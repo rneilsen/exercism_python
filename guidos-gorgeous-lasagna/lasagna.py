6cec4ccce29ef09e0a571f9234e709bfdb5c31d5
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-09-03 17:07:26 +1000

    Download Guido's Gorgeous Lasagna exercise

---
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -0,0 +1,20 @@
+# TODO: define the 'EXPECTED_BAKE_TIME' constant
+# TODO: define the 'PREPARATION_TIME' constant
+
+
+# TODO: define the 'bake_time_remaining()' function
+def bake_time_remaining():
+    '''
+    :param elapsed_bake_time: int baking time already elapsed
+    :return: int remaining bake time derived from 'EXPECTED_BAKE_TIME'
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    '''
+
+    pass
+
+# TODO: define the 'preparation_time_in_minutes()' function
+
+# TODO: define the 'elapsed_time_in_minutes()' function
