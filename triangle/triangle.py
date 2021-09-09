b05689b2c5ace7a8bf8c852d830f08ba9efbce1e
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-09-09 13:38:44 +1000

    Cleanup style issues, factor out num_distinct func

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,21 +1,29 @@
+def num_distinct(stuff):
+    return len(set(stuff))
+
+
 def equilateral(sides):
-    if not test_valid(sides): return False
-    return len(set(sides)) == 1
+    if not test_valid(sides): 
+        return False
+    return num_distinct(sides) == 1
 
 
 def isosceles(sides):
-    if not test_valid(sides): return False
-    return len(set(sides)) <= 2
+    if not test_valid(sides): 
+        return False
+    return num_distinct(sides) <= 2
 
 
 def scalene(sides):
-    if not test_valid(sides): return False
-    return len(set(sides)) == 3
+    if not test_valid(sides): 
+        return False
+    return num_distinct(sides) == 3
 
 
 def degenerate(sides):
-    if not test_valid(sides): return False
-    s_sides = sides.sort()
+    if not test_valid(sides): 
+        return False
+    s_sides = sorted(sides)
     return s_sides[0] + s_sides[1] == s_sides[2]
 
 
