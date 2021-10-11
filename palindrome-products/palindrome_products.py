70da7fee1fe8f66d49b230a309031a3598823023
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-11 16:25:04 +1100

    Redesign with new, more efficient algorithm

--- palindrome-products/palindrome_products.py
+++ palindrome-products/palindrome_products.py
@@ -1,30 +1,53 @@
 def largest(min_factor, max_factor):
-    return find_best(min_factor, max_factor, lambda x,y: x > y)
-
+    best = find_best(min_factor, max_factor, smallest=False)
+    return (best, find_factors(best, min_factor, max_factor))
+    
 
 def smallest(min_factor, max_factor):
-    return find_best(min_factor, max_factor, lambda x,y: x < y)
+    best = find_best(min_factor, max_factor, smallest=True)
+    return (best, find_factors(best, min_factor, max_factor))
 
 
-def find_best(min_factor, max_factor, comp):
-    # uses the 'comp' function to determine whether to throw out previous best
-    # if comp(new_prod, prev_best) is True, then new_prod becomes the new best
+def find_best(min_factor, max_factor, smallest):
     if min_factor > max_factor:
         raise ValueError("min_factor larger than max_factor")
+    
+    if smallest:
+        (start, stop, step) = (min_factor, max_factor + 1, 1)
+    else:
+        (start, stop, step) = (max_factor, min_factor - 1, -1)
+
+    i = start
     best = None
-    factors = []
-    for i in range(min_factor, max_factor+1):
-        for j in range(i, max_factor+1):
-            test = i*j
+    while i != stop:
+        j = start
+        jstop = i + step
+        while j != jstop:
             if is_palindrome(i*j):
-                if best is None or comp(test, best): 
-                    best = test
-                    factors = [[i,j]]
-                elif test == best:
-                    factors.append([i,j])
+                if best is None:
+                    best = i*j
+                elif smallest:
+                    best = min(best, i*j)
+                else:
+                    best = max(best, i*j)
+            j += step
+        i += step
+        if best is not None:
+            # abort search if i*(starting j value) can't ever surpass best
+            if (smallest and i * start > best) or (not smallest and i * start < best):
+                break
+    return best
 
 
-    return (best, factors)
+def find_factors(num, min, max):
+    if num is None:
+        return []
+    
+    factors = []
+    for i in range(min, max + 1):
+        if num % i == 0 and num // i in range(min, max + 1):
+            factors.append((i, num // i))
+    return factors
 
 
 def is_palindrome(number):
