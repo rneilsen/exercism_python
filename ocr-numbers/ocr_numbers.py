995d7f1e25b9320a595205b9a6854e1dbee77b03
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-11 14:10:35 +1100

    Implement one-digit recognition for convert()

--- ocr-numbers/ocr_numbers.py
+++ ocr-numbers/ocr_numbers.py
@@ -1,2 +1,12 @@
+# Create a dict (DIGITS): key = each digit as pipes and underscores, val = integer
+
+RAW_DIGITS = '''
+ _     _  _     _  _  _  _  _ 
+| |  | _| _||_||_ |_   ||_||_|
+|_|  ||_  _|  | _||_|  ||_| _|
+                              '''
+DIGITS = {'\n'.join( [row[3*n:3*n+3] for row in RAW_DIGITS.splitlines()[1:]] ) : str(n)
+        for n in range(10)}
+
 def convert(input_grid):
-    pass
+    return DIGITS['\n'.join(input_grid)]
