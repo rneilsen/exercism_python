41c5f3b441c0fbe073dbe4da123a6db3c29601e0
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-11 14:15:50 +1100

    Implement checks for invalid input_grid size

--- ocr-numbers/ocr_numbers.py
+++ ocr-numbers/ocr_numbers.py
@@ -9,4 +9,10 @@ DIGITS = {'\n'.join( [row[3*n:3*n+3] for row in RAW_DIGITS.splitlines()[1:]] ) :
         for n in range(10)}
 
 def convert(input_grid):
+    if len(input_grid) < 4 or len(input_grid) % 4 != 0:
+        raise ValueError("Invalid input grid size")
+    lengths = [len(line) for line in input_grid]
+    if len(set(lengths)) != 1 or lengths[0] % 3 != 0:
+        raise ValueError("Invalid input grid size")
+
     return DIGITS['\n'.join(input_grid)]
