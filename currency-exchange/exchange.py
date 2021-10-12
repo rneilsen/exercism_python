f44c39ba592275e17290f96b3e251995c2b2e9c9
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-12 15:20:14 +1100

    Implement exchange_money() function

--- currency-exchange/exchange.py
+++ currency-exchange/exchange.py
@@ -6,7 +6,7 @@ def exchange_money(budget, exchange_rate):
     :return: float - exchanged value of the foreign currency you can receive.
     """
 
-    pass
+    return budget / exchange_rate
 
 
 def get_change(budget, exchanging_value):
