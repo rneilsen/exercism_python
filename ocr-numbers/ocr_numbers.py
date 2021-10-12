4f684e8009dbe2808abfbbe3f00ff07d1c8e03ca
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-12 12:57:24 +1100

    Add more specific exception messages

--- ocr-numbers/ocr_numbers.py
+++ ocr-numbers/ocr_numbers.py
@@ -4,23 +4,26 @@ RAW_DIGITS = '''
 | |  | _| _||_||_ |_   ||_||_|
 |_|  ||_  _|  | _||_|  ||_| _|
                               '''
-
 DIGITS = {}
 for n in range(10):
     digit_rows = [row[3*n:3*n+3] for row in RAW_DIGITS.splitlines()[1:]]
     DIGITS['\n'.join(digit_rows)] = str(n)
 
 def convert(input_grid):
-    if len(input_grid) < 4 or len(input_grid) % 4 != 0:
-        raise ValueError("Invalid input grid size")
+    if len(input_grid) < 4: 
+        raise ValueError("Input does not have at least 4 rows")
+    if len(input_grid) % 4 != 0:
+        raise ValueError("Input does not have multiple of 4 rows")
     
     big_rows = [input_grid[n:n+4] for n in range(0, len(input_grid), 4)]
     result = ''
     for big_row in big_rows:
         # check all lines in this big_row are same mult-of-3 length
         lengths = [len(line) for line in big_row]
-        if len(set(lengths)) != 1 or lengths[0] % 3 != 0:
-            raise ValueError("Invalid input grid size")
+        if len(set(lengths)) != 1:
+            raise ValueError("Rows in a set of 4 not the same length")
+        if lengths[0] % 3 != 0:
+            raise ValueError("Rows are not multiple-of-three in length")
 
         # peel off each big_digit in big_row and identify
         for i in range(0, len(big_row[0]), 3):
