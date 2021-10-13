a6bb4f12d7f03acf53f5c2f54705a75a33439ff7
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-13 16:17:48 +1100

    Implement basic encode functionality
    Still needs to add spaces every 5 chars

--- affine-cipher/affine_cipher.py
+++ affine-cipher/affine_cipher.py
@@ -8,6 +8,14 @@ ALPHNUM = {ALPHABET[n]: n for n in range(M)}
 def encode(plain_text, a, b):
     if gcd(a, M) != 1:
         raise ValueError("a must be coprime to m")
+    
+    cipher_text = ""
+    for ch in plain_text.lower():
+        if ch not in ALPHABET:
+            continue
+        cipher_text += NUMALPH[(a * ALPHNUM[ch] + b) % M]
+
+    return cipher_text
 
 
 def decode(ciphered_text, a, b):
