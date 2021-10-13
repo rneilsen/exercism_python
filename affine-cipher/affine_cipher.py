d7a4c50bc858eab6e54b2e5ea44b98aef88cb8a1
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-13 16:12:46 +1100

    Implement check for a, m coprime

--- affine-cipher/affine_cipher.py
+++ affine-cipher/affine_cipher.py
@@ -1,6 +1,15 @@
+from string import ascii_lowercase as ALPHABET
+from math import gcd
+
+M = len(ALPHABET)
+NUMALPH = {n: ALPHABET[n] for n in range(M)}
+ALPHNUM = {ALPHABET[n]: n for n in range(M)}
+
 def encode(plain_text, a, b):
-    pass
+    if gcd(a, M) != 1:
+        raise ValueError("a must be coprime to m")
 
 
 def decode(ciphered_text, a, b):
-    pass
+    if gcd(a, M) != 1:
+        raise ValueError("a must be coprime to m")
