0da4e431a98843dd92c7bd256a9dc323fe0bd1c3
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-14 14:00:36 +1100

    Implement grouping in encode() and cleanup flow

--- affine-cipher/affine_cipher.py
+++ affine-cipher/affine_cipher.py
@@ -1,6 +1,8 @@
 from string import ascii_lowercase as ALPHABET
 from math import gcd
 
+GROUP_SIZE = 5
+
 M = len(ALPHABET)
 NUMALPH = {n: ALPHABET[n] for n in range(M)}
 ALPHNUM = {ALPHABET[n]: n for n in range(M)}
@@ -11,11 +13,13 @@ def encode(plain_text, a, b):
     
     cipher_text = ""
     for ch in plain_text.lower():
-        if ch not in ALPHABET:
-            continue
-        cipher_text += NUMALPH[(a * ALPHNUM[ch] + b) % M]
-
-    return cipher_text
+        if ch in ALPHABET:
+            cipher_text += NUMALPH[(a * ALPHNUM[ch] + b) % M]
+        elif ch in '0123456789':
+            cipher_text += ch
+        
+    return ' '.join(cipher_text[i:i+GROUP_SIZE] 
+            for i in range(0, len(cipher_text), GROUP_SIZE))
 
 
 def decode(ciphered_text, a, b):
