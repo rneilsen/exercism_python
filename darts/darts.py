fa33e3f4fc0cef21f17d918adeabd418a6e875a8
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-26 15:03:50 +1100

    Implement score() function

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,11 @@
+from math import sqrt
+
 def score(x, y):
-    pass
+    radius = sqrt(x**2 + y**2)
+    if radius <= 1:
+        return 10
+    if radius <= 5:
+        return 5
+    if radius <= 10:
+        return 1
+    return 0
