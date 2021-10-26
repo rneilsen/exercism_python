f597c8bc61344f7e9e63e045ff0b60f52a1919c9
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-26 14:26:37 +1100

    Fix pylint warnings

--- ghost-gobble-arcade-game/arcade_game.py
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -1,3 +1,5 @@
+"""Implements boolean functions for determining game states in Pacman"""
+
 def eat_ghost(power_pellet_active, touching_ghost):
     """
 
@@ -5,7 +7,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :param touching_ghost:  bool - is the player touching a ghost?
     :return: bool
     """
-    return (power_pellet_active and touching_ghost)
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
@@ -15,7 +17,7 @@ def score(touching_power_pellet, touching_dot):
     :param touching_dot:  bool - is the player touching a dot?
     :return: bool
     """
-    return (touching_power_pellet or touching_dot)
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -25,7 +27,7 @@ def lose(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool
     """
-    return (not power_pellet_active and touching_ghost)
+    return not power_pellet_active and touching_ghost
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -36,4 +38,4 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :param touching_ghost:  bool - is the player touching a ghost?
     :return: bool
     """
-    return (has_eaten_all_dots and not lose(power_pellet_active, touching_ghost))
+    return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost)
