dd5e1b3386195f4a4452d6a01fd3b8212cd7e383
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-27 16:09:37 +1100

    Implement get_neighbours() function

--- connect/connect.py
+++ connect/connect.py
@@ -1,7 +1,22 @@
+from itertools import product
 
 class ConnectGame:
     def __init__(self, board):
-        pass
+        self.board = [row.strip().split(' ') for row in board.splitlines()]
+        self.height = len(self.board)
+        self.width = len(self.board[0])
+    
+
+    def get_neighbours(self, row, col):
+        standard_steps = {(-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0)}
+        standard_neighbours = {(row + rstep, col + cstep) for (rstep, cstep) in standard_steps}
+
+        valid_neighbours = set(product(
+                range(max(0, row - 1), 1 + min(self.height - 1, row + 1)),
+                range(max(0, col - 1), 1 + min(self.width - 1, col + 1)) ))
+
+        return standard_neighbours.intersection(valid_neighbours)
+
 
     def get_winner(self):
         pass
