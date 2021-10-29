2401613f5059c9d31a3928f46b538652a2d3e354
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-10-29 18:30:41 +1100

    Implement atbash-cipher

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,18 @@
+from string import ascii_lowercase as alphabet
+
+GROUP_SIZE = 5
+
 def encode(plain_text):
-    pass
+    cipher = {pt: ct for (pt, ct) in zip(alphabet, reversed(alphabet))}
+    cipher.update({ str(n): str(n) for n in range(10) })
+
+    cipher_text = ''
+    cipher_text += ''.join([cipher[ch] for ch in plain_text.lower()
+            if ch.isalnum()])
+
+    return ' '.join([cipher_text[i:i+GROUP_SIZE] 
+            for i in range(0, len(cipher_text), GROUP_SIZE)])
 
 
 def decode(ciphered_text):
-    pass
+    return encode(ciphered_text).replace(' ', '')
