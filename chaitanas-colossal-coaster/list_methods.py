810d4a29029aee67a6a5f872ad576a2d10b7519b
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-03 17:04:12 +1100

    Implemented list_methods.py functions

--- chaitanas-colossal-coaster/list_methods.py
+++ chaitanas-colossal-coaster/list_methods.py
@@ -7,7 +7,9 @@ def add_me_to_the_queue(express_queue, normal_queue, ticket_type, person_name):
     :param person_name: str - name of person to add to a queue.
     :return: list - the (updated) queue the name was added to.
     """
-    pass
+    if ticket_type == 1:
+        return express_queue + [person_name]
+    return normal_queue + [person_name]
 
 
 def find_my_friend(queue, friend_name):
@@ -17,7 +19,7 @@ def find_my_friend(queue, friend_name):
     :param friend_name: str - name of friend to find.
     :return: int - index at which the friends name was found.
     """
-    pass
+    return queue.index(friend_name)
 
 
 def add_me_with_my_friends(queue, index, person_name):
@@ -28,7 +30,7 @@ def add_me_with_my_friends(queue, index, person_name):
     :param person_name: str - the name to add.
     :return: list - queue updated with new name.
     """
-    pass
+    return queue[:index] + [person_name] + queue[index:]
 
 
 def remove_the_mean_person(queue, person_name):
@@ -38,7 +40,8 @@ def remove_the_mean_person(queue, person_name):
     :param person_name: str - name of mean person.
     :return:  list - queue update with the mean persons name removed.
     """
-    pass
+    index = queue.index(person_name)
+    return queue[:index] + queue[index + 1:]
 
 
 def how_many_namefellows(queue, person_name):
@@ -48,7 +51,7 @@ def how_many_namefellows(queue, person_name):
     :param person_name: str - name you wish to count or track.
     :return:  int - the number of times the name appears in the queue.
     """
-    pass
+    return queue.count(person_name)
 
 
 def remove_the_last_person(queue):
@@ -57,7 +60,7 @@ def remove_the_last_person(queue):
     :param queue: list - names in the queue.
     :return: str - name that has been removed from the end of the queue.
     """
-    pass
+    return queue[-1]
 
 
 def sorted_names(queue):
@@ -66,4 +69,4 @@ def sorted_names(queue):
     :param queue: list - names in the queue.
     :return: list - copy of the queue in alphabetical order.
     """
-    pass
+    return sorted(queue)
