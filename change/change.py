161d3225e56b2ebe7605123781ed9126d2f912db
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-04 14:35:37 +1100

    Greedy algorithm, only works for canonical coins

--- change/change.py
+++ change/change.py
@@ -1,2 +1,14 @@
 def find_fewest_coins(coins, target):
-    pass
+    coins = sorted(coins)
+    change = []
+
+    if target < 0:
+        raise ValueError("target can't be negative")
+    while target > 0 and len(coins) > 0:
+        coin = coins.pop()
+        while coin <= target:
+            target -= coin
+            change.append(coin)
+    if target != 0:
+        raise ValueError("can't make target with given coins")
+    return change
