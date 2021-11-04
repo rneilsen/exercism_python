20b2829e6b30ab53d172a6f766bf79da973473d3
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-04 15:14:18 +1100

    Implement via breadth-first search

--- change/change.py
+++ change/change.py
@@ -1,14 +1,16 @@
-def find_fewest_coins(coins, target):
-    coins = sorted(coins)
-    change = []
+from itertools import combinations_with_replacement
 
+def find_fewest_coins(coins, target):
     if target < 0:
         raise ValueError("target can't be negative")
-    while target > 0 and len(coins) > 0:
-        coin = coins.pop()
-        while coin <= target:
-            target -= coin
-            change.append(coin)
-    if target != 0:
-        raise ValueError("can't make target with given coins")
-    return change
+
+    max_coins = 1 + target // min(coins)
+    i = 0
+    while i < max_coins:
+        combos = combinations_with_replacement(coins, i)
+        for combo in combos:
+            if sum(combo) == target:
+                return sorted(combo)
+        i += 1
+
+    raise ValueError("can't make target with given coins")
