4a718a6b59e2b39942f8989e0c0435757b6be1d0
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-08 15:22:19 +1100

    Replace while loop with for loop

--- change/change.py
+++ change/change.py
@@ -5,12 +5,11 @@ def find_fewest_coins(coins, target):
         raise ValueError("target can't be negative")
 
     max_coins = 1 + target // min(coins)
-    i = 0
-    while i < max_coins:
+    
+    for i in range(max_coins):
         combos = combinations_with_replacement(coins, i)
         for combo in combos:
             if sum(combo) == target:
                 return sorted(combo)
-        i += 1
 
     raise ValueError("can't make target with given coins")
