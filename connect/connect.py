4c920a639503f1f650425a719846abad3136ea97
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-08 14:31:40 +1100

    Reengineer connectivity checking
    Instead of finding the entire contiguous block (may be wasteful in some
    cases) we now use a function that asks if two given sets are connected
    by a contiguous block containing a given symbol. This function can
    terminate the search early, as soon as it finds a connection, and not
    waste its time finding more of the contiguous block.

--- connect/connect.py
+++ connect/connect.py
@@ -13,53 +13,54 @@ class ConnectGame:
 
         valid_neighbours = set(product(
                 range(max(0, row - 1), 1 + min(self.height - 1, row + 1)),
-                range(max(0, col - 1), 1 + min(self.width - 1, col + 1)) ))
+                range(max(0, col - 1), 1 + min(self.width - 1, col + 1)) 
+        ))
 
         return standard_neighbours.intersection(valid_neighbours)
 
 
-    def get_connected_alike_block(self, row, col):
-        search_type = self.board[row][col]
-        check_queue = list(self.get_neighbours(row, col))
-        checked_cells = {(row, col)}
-        connected_alike_block = {(row, col)}
+    def are_groups_connected_by(self, symbol, set1, set2):
+        """Tests whether there is a contiguous block of cells containing a 
+        given symbol that contains cells in both groups"""
+        set1 = {(r, c) for (r, c) in set1 if self.board[r][c] == symbol}
+        set2 = {(r, c) for (r, c) in set2 if self.board[r][c] == symbol}
 
-        while len(check_queue) > 0:
-            (check_row, check_col) = check_queue.pop()
-            checked_cells.add((check_row, check_col))
-            
-            if self.board[check_row][check_col] == search_type:
-                connected_alike_block.add((check_row, check_col))
-                check_neighbours = self.get_neighbours(check_row, check_col)
-                check_queue.extend(check_neighbours.difference(checked_cells))
-        
-        return connected_alike_block
+        while len(set1) > 0:
+            checked_cells = set()
+            connected_alike_block = set()
+
+            check_cell = set1.pop()
+            check_queue = {check_cell}
+
+            while len(check_queue) > 0:
+                check_row, check_col = check_queue.pop()
+                checked_cells.add((check_row, check_col))
+
+                if self.board[check_row][check_col] == symbol:
+                    if (check_row, check_col) in set2:
+                        return True
+
+                    connected_alike_block.add((check_row, check_col))
+                    check_neighbours = self.get_neighbours(check_row, check_col)
+                    check_queue.update(check_neighbours - checked_cells)
+
+            set1 -= checked_cells
+
+        return False
 
 
     def get_winner(self):
         # Check for O win (top to bottom)
         top_row = {(0,i) for i in range(self.width)}
         bottom_row = {(self.height - 1, i) for i in range(self.width)}
-        while len(top_row) > 0:
-            (row, col) = top_row.pop()
-            if self.board[row][col] == 'O':
-                block = self.get_connected_alike_block(row, col)
-                if len(block.intersection(bottom_row)) != 0:
-                    return "O"
-                else:
-                    top_row.difference_update(block)
+        if self.are_groups_connected_by('O', top_row, bottom_row):
+            return 'O'
 
         # Check for X win (left to right)
         left_col = {(i, 0) for i in range(self.height)}
         right_col = {(i, self.width - 1) for i in range(self.height)}
-        while len(left_col) > 0:
-            (row, col) = left_col.pop()
-            if self.board[row][col] == 'X':
-                block = self.get_connected_alike_block(row, col)
-                if len(block.intersection(right_col)) != 0:
-                    return "X"
-                else:
-                    left_col.difference_update(block)
-        
+        if self.are_groups_connected_by('X', left_col, right_col):
+            return 'X'
+
         # No win yet
-        return ""
+        return ''
