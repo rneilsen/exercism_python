14d2d12abec7f5521426192a4412ab7f5fcfcc7a
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-09 14:56:06 +1100

    Use int division instead of float

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,11 +1,11 @@
 def square_of_sum(number):
     # square of (n^2+n)/2 formula
-    return ((number**2 + number) / 2)**2
+    return ((number**2 + number) // 2)**2
 
 
 def sum_of_squares(number):
     # closed form of square pyramidal number
-    return number * (number+1) * (2*number+1) / 6
+    return number * (number + 1) * (2*number + 1) // 6
 
 
 def difference_of_squares(number):
