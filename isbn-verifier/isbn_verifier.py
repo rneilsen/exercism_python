fc2f8f4981559b840508d397cd4bf9f431c8763d
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-09 15:12:21 +1100

    Simplify preliminary checks & cleanup

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,19 +1,18 @@
 def is_valid(isbn):
-    scrubbed = list(filter(lambda x: x != '-', isbn))
+    scrubbed = isbn.replace('-', '')
 
     if len(scrubbed) != 10:
         return False
-    for c in scrubbed[:-1]:
-        if not c.isdigit():
-            return False
-    
+    if not scrubbed[:-1].isnumeric():
+        return False
+
     digits = [int(c) for c in scrubbed[:-1]]
-    
+
     if scrubbed[-1].isdigit():
         digits.append(int(scrubbed[-1]))
     elif scrubbed[-1] == 'X':
         digits.append(10)
     else:
         return False
-    
+
     return (sum([x*i for (x,i) in zip(digits, range(10,0,-1))]) % 11 == 0)
