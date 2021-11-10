6bed7f55eb4336ceef75dff537906b3d5b7ae00e
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-10 16:49:45 +1100

    Refactor with list-based construction

--- say/say.py
+++ say/say.py
@@ -1,55 +1,50 @@
-names = { 0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 
-            6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 
-            11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 
+names = { 0: '', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+            6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+            11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
             15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen',
-            19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty', 
+            19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty',
             50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'}
 
-powers = {0: '', 3: ' thousand', 6: ' million', 9: ' billion'}
+powers = {0: '', 3: 'thousand', 6: 'million', 9: 'billion'}
 
 
 def say(number):
+    """Returns the English name of a given integer"""
     if not (0 <= number < 10**12):
         raise ValueError("Number out of range")
     if number == 0:
         return 'zero'
 
-    return rec_say(number)
+    words = rec_say(number)
+    while '' in words:
+        words.remove('')
+
+    return ' '.join(words)
 
 
 def rec_say(number):
+    """Constructs the name of a positive integer as a list of words"""
     if number == 0:
-        return ''
+        return []
 
     n = 0
     while number >= 1000**(n+1):
         n += 1
 
-    if number < 1000:
-        return block_name(number)
+    this_block = number // (1000**n)
+    remainder = number % 1000**n
 
-    this_block_name = block_name(number // (1000**n)) + powers[3*n]
-    remainder_name = rec_say(number % 1000**n)
-
-    return this_block_name + (' ' + remainder_name if remainder_name else '')
+    return block_name(this_block) + [powers[3*n]] + rec_say(remainder)
 
 
 def block_name(number):
-    """names numbers from 1 to 999 inclusive"""
-    if not 1 <= number <= 999:
-        raise ValueError
-
-    name = ''
-
+    """Constructs name of integer from 1 to 999 inclusive as a list of words"""
+    name = []
     if number >= 100:
-        name += names[number // 100] + ' hundred'
+        name += [names[number // 100], 'hundred']
         number %= 100
 
-    if number == 0: return name
-
-    if name: name += ' '
-
     if number in names:
-        return name + names[number]
+        return name + [names[number]]
 
-    return name + names[number - number % 10] + '-' + names[number % 10]
+    return name + [names[10 * (number // 10)] + '-' + names[number % 10]]
