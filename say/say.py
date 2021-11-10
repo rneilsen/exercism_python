81db3f9b32abefc0b5860d41906a2fc5d735b470
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-10 16:22:00 +1100

    Implement recursive solution

--- say/say.py
+++ say/say.py
@@ -7,31 +7,49 @@ names = { 0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
 
 powers = {0: '', 3: ' thousand', 6: ' million', 9: ' billion'}
 
-# construct 'names' dict for all numbers up to 1000
-for n in range(21, 100): 
-    if n not in names:
-        names[n] = names[10 * (n//10)] + '-' + names[n % 10]
-
-for n in range(10):
-    names[100*n] = names[n] + ' hundred'
-
-for n in range(100, 1000):
-    if n not in names:
-        names[n] = names[100 * (n//100)] + ' ' + names[n % 100]
-
 
 def say(number):
-    if not (0 <= number < 10**12): raise ValueError("Number out of range")
+    if not (0 <= number < 10**12):
+        raise ValueError("Number out of range")
+    if number == 0:
+        return 'zero'
 
-    if number == 0: return 'zero'
-    
-    parts = []
-    
-    for p in powers.keys():
-        block = number % 1000
-        if block > 0:
-            parts.append(names[block] + powers[p])
-        number = number // 1000
-    
-    parts.reverse()
-    return ' '.join(parts)
+    return rec_say(number)
+
+
+def rec_say(number):
+    if number == 0:
+        return ''
+
+    n = 0
+    while number >= 1000**(n+1):
+        n += 1
+
+    if number < 1000:
+        return block_name(number)
+
+    this_block_name = block_name(number // (1000**n)) + powers[3*n]
+    remainder_name = rec_say(number % 1000**n)
+
+    return this_block_name + (' ' + remainder_name if remainder_name else '')
+
+
+def block_name(number):
+    """names numbers from 1 to 999 inclusive"""
+    if not 1 <= number <= 999:
+        raise ValueError
+
+    name = ''
+
+    if number >= 100:
+        name += names[number // 100] + ' hundred'
+        number %= 100
+
+    if number == 0: return name
+
+    if name: name += ' '
+
+    if number in names:
+        return name + names[number]
+
+    return name + names[number - number % 10] + '-' + names[number % 10]
