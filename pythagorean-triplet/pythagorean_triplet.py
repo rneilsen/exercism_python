dea56588457f14af5e129a532015786d70f1477a
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-17 16:11:51 +1100

    Implement much faster algorithm

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,10 +1,25 @@
-def triplets_with_sum(number):
+from typing import List
+
+def triplets_with_sum(number: int) -> List[List[int]]:
+    """Returns a list of all Pythagorean triples having the given sum"""
     triplets = []
-    for a in range(3, number // 3):
-        for b in range(a+1, 2 * (number // 3)):
-            c = number - (a+b)
-            if a**2 + b**2 == c**2:
-                triplets.append([a,b,c])
-            elif a**2 + b**2 > c**2:
-                break
+    primitive_queue = [[3,4,5]]
+    while len(primitive_queue) > 0:
+        (a,b,c) = primitive_queue.pop()
+        if number % sum((a,b,c)) == 0:
+            k = number // sum((a,b,c))
+            triplets.append(sorted([k*a, k*b, k*c]))
+
+        children = calc_child_primitives(a,b,c)
+        for child in children:
+            if sum(child) <= number:
+                primitive_queue.append(child)
     return triplets
+
+def calc_child_primitives(a,b,c) -> List[List[int]]:
+    """Uses the Berggren transformations to produce primitive pythagorean
+    triples from a given triple, the resulting tree includes every primitive
+    triple exactly once"""
+    return [[a - 2*b + 2*c, 2*a - b + 2*c, 2*a - 2*b + 3*c],
+            [a + 2*b + 2*c, 2*a + b + 2*c, 2*a + 2*b + 3*c],
+            [-a + 2*b + 2*c, -2*a + b + 2*c, -2*a + 2*b + 3*c]]
