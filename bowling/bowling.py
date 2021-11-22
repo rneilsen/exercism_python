08645bcc86b6364acb6d433f2d74481e838f6be1
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-11-22 16:30:34 +1100

    Minor cleanup, add comments & docstrings

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -4,20 +4,23 @@ NUM_FRAMES = 10
 NUM_PINS = 10
 
 class BowlingGame:
+    """Tracks the score for a single player's bowling game"""
     def __init__(self):
         self.total = 0
-        self.frame_prev = None
+        self.prev_pinfall_in_frame = None
         self.cur_frame = 1
         self.multipliers = deque([1,1])
         self.last_frame_bowls = []
         self.game_over = False
 
     def roll(self, pins):
+        """Add a new roll to this game. Rejects invalid rolls, updates score"""
         if self.game_over:
             raise Exception("Extra bowls not allowed")
         if not 0 <= pins <= NUM_PINS:
             raise Exception("Invalid pinfall")
-        if self.frame_prev is not None and self.frame_prev + pins > NUM_PINS:
+        if self.prev_pinfall_in_frame is not None and \
+                self.prev_pinfall_in_frame + pins > NUM_PINS:
             raise Exception("Invalid frame total")
 
         # Add pins using bonus multiplier from previous strikes & spares
@@ -34,31 +37,33 @@ class BowlingGame:
             elif len(self.last_frame_bowls) == 2:
                 if self.last_frame_bowls[0] == NUM_PINS:
                     if pins < NUM_PINS:
-                        self.frame_prev = pins
+                        # first bowl strike, second bowl not, retain score
+                        self.prev_pinfall_in_frame = pins
                 elif sum(self.last_frame_bowls) == NUM_PINS:
-                    self.frame_prev = None
+                    # first two bowls formed a spare (1 fill ball)
+                    self.prev_pinfall_in_frame = None
                 else:
+                    # first two bowls were not a spare, game over
                     self.game_over = True
             else:
                 if pins < NUM_PINS:
-                    self.frame_prev = pins
+                    # first bowl was not a strike, retain score
+                    self.prev_pinfall_in_frame = pins
             return
 
         # Multiplier incrementing for strikes & spares
-        if self.frame_prev is not None:
+        if self.prev_pinfall_in_frame is not None:
             # this is the second bowl of a frame
-            if self.frame_prev + pins == NUM_PINS:
-                # spare!
+            if self.prev_pinfall_in_frame + pins == NUM_PINS:  # spare!
                 self.multipliers[0] += 1
-            self.frame_prev = None
+            self.prev_pinfall_in_frame = None
             self.cur_frame += 1
-        elif pins == NUM_PINS:
-            # strike!:
+        elif pins == NUM_PINS:  # strike!:
             self.multipliers[0] += 1
             self.multipliers[1] += 1
             self.cur_frame += 1
         else:
-            self.frame_prev = pins
+            self.prev_pinfall_in_frame = pins
 
 
     def score(self):
