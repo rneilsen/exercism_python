773df05ac1ad1ae65d9cdd4725389f5b83a3ec98
Richard Neilsen <RNeilsen@users.noreply.github.com>
2022-01-03 14:38:46 +1100

    Small cleanup

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -7,12 +7,14 @@ class BowlingGame:
     """Tracks the score for a single player's bowling game"""
     def __init__(self):
         self.total = 0                      # running total score
-        self.prev_pinfall_in_frame = None   # stores the previous pinfall in
-                                            # the current frame (for spares etc)
         self.cur_frame = 1                  # number of the current frame
-        self.multipliers = deque([1,1])     # store multipliers for upcoming rolls
-        self.last_frame_bowls = []          # stores pinfalls in the last frame
-        self.game_over = False
+        self.game_over = False              # track whether this game has ended
+
+        self._multipliers = deque([1,1])    # stores multipliers for upcoming rolls
+        self._last_frame_bowls = []         # stores pinfalls in the last frame
+        self._prev_pinfall_in_frame = None  # stores the previous pinfall in
+                                            # the current frame (for spares etc)
+
 
     def roll(self, pins):
         """Add a new roll to this game. Rejects invalid rolls, updates score"""
@@ -20,54 +22,61 @@ class BowlingGame:
             raise IndexError("Extra bowls not allowed")
         if not 0 <= pins <= NUM_PINS:
             raise ValueError("Invalid pinfall")
-        if self.prev_pinfall_in_frame is not None and \
-                self.prev_pinfall_in_frame + pins > NUM_PINS:
+        if self._prev_pinfall_in_frame is not None and \
+                self._prev_pinfall_in_frame + pins > NUM_PINS:
             raise ValueError("Invalid frame total")
 
         # Add pins using bonus multiplier from previous strikes & spares
-        mult = self.multipliers.popleft()
-        self.multipliers.append(1)
+        mult = self._multipliers.popleft()
+        self._multipliers.append(1)
         self.total += mult * pins
 
         # If we're on the last frame, handle it differently
-        if self.cur_frame == NUM_FRAMES:
+        if self._is_last_frame():
             self._handle_last_frame(pins)
             return
 
         # Multiplier incrementing for strikes & spares
-        if self.prev_pinfall_in_frame is not None:
+        if self._prev_pinfall_in_frame is not None:
             # this is the second bowl of a frame
-            if self.prev_pinfall_in_frame + pins == NUM_PINS:  # spare!
-                self.multipliers[0] += 1
-            self.prev_pinfall_in_frame = None
+            if self._prev_pinfall_in_frame + pins == NUM_PINS:  # spare!
+                self._multipliers[0] += 1
+            self._prev_pinfall_in_frame = None
             self.cur_frame += 1
         elif pins == NUM_PINS:  # strike!:
-            self.multipliers[0] += 1
-            self.multipliers[1] += 1
+            self._multipliers[0] += 1
+            self._multipliers[1] += 1
             self.cur_frame += 1
         else:
-            self.prev_pinfall_in_frame = pins
+            # this is the first bowl of a frame
+            self._prev_pinfall_in_frame = pins
+
+
+    def _is_last_frame(self):
+        return self.cur_frame == NUM_FRAMES
+
 
     def _handle_last_frame(self, pins):
-        self.last_frame_bowls.append(pins)
+        self._last_frame_bowls.append(pins)
 
-        if len(self.last_frame_bowls) == 3:
+        if len(self._last_frame_bowls) == 3:
             self.game_over = True
-        elif len(self.last_frame_bowls) == 2:
-            if self.last_frame_bowls[0] == NUM_PINS:
+        elif len(self._last_frame_bowls) == 2:
+            if self._last_frame_bowls[0] == NUM_PINS:
                 if pins < NUM_PINS:
                     # first bowl strike, second bowl not, retain score
-                    self.prev_pinfall_in_frame = pins
-            elif sum(self.last_frame_bowls) == NUM_PINS:
+                    self._prev_pinfall_in_frame = pins
+            elif sum(self._last_frame_bowls) == NUM_PINS:
                 # first two bowls formed a spare (1 fill ball)
-                self.prev_pinfall_in_frame = None
+                self._prev_pinfall_in_frame = None
             else:
                 # first two bowls were not a spare, game over
                 self.game_over = True
         else:
             if pins < NUM_PINS:
                 # first bowl was not a strike, retain score
-                self.prev_pinfall_in_frame = pins
+                self._prev_pinfall_in_frame = pins
+
 
     def score(self):
         return self.total
